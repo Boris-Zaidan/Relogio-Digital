85bc59e82ae16822512c2862ce45847047435724
Boris zaidan <Bzmgs31@gmail.com>
2024-08-08 08:42:54 -0300

    js
    fiz um Update no código aplicado melhorias é um código mais limpo.

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const Horas = document.querySelector('#horas');
-const Minutos = document.querySelector('#minutos');
-const Segundos = document.querySelector('#segundos')
+function atualizarRelogio () {
+  const dateToday = new Date();
 
-const relogio = setInterval ( function time() {
-  let dateToday = new Date();
-  let hr = dateToday.getHours();
-  let min = dateToday.getMinutes();
-  let seg = dateToday.getSeconds();
+  //padStart(2, '0') garante que qualquer número menor que 10 será formatado com um zero à esquerda.
+  const hr = String(dateToday.getHours()).padStart(2, '0');
+  const min = String(dateToday.getMinutes()).padStart(2, '0');
+  const seg = String(dateToday.getSeconds()).padStart(2, '0');
 
-  if (hr < 10) hr = '0' + hr;
-  if (min < 10) min = '0' + min;
-  if (seg < 10 ) seg = '0' + seg
+  document.querySelector('#horas').textContent = hr;
+  document.querySelector('#minutos').textContent = min;
+  document.querySelector('#segundos').textContent = seg;
 
-  Horas.textContent = hr
-  Minutos.textContent = min
-  Segundos.textContent = seg
+}
+  // Definir o intervalo para atualizar o relógio a cada 1 segundo
+  const relogio = setInterval(atualizarRelogio, 1000);
+  
+  atualizarRelogio() // Atualiza imediatamente, sem esperar 1 segundo
 
-} )
